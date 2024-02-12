316827010b832e5408dc57ebec97255a42a6cdb3
Jena <jenagansivakumar@gmail.com>
2024-02-12 18:21:30 +0000

    init commit

---
+++ src/components/accordion/data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    id: "1",
+    question: "lorem",
+    answer: "ipsem",
+  },
+  {
+    id: "2",
+    question: "lorem 2",
+    answer: "ipsem2",
+  },
+  {
+    id: "3",
+    question: "lorem 3",
+    answer: "ipsem3",
+  },
+  {
+    id: "4",
+    question: "lorem 4",
+    answer: "ipsem4",
+  },
+];
