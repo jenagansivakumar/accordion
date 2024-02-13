dbdd0b67da7a9f29be0f91960af2f36c6d037859
Jena <jenagansivakumar@gmail.com>
2024-02-13 15:24:14 +0000

    added on click to the titles

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,21 @@ import data from "./components/accordion/data";
 
 function App() {
   const [selected, setSelected] = useState(null);
-
+  function handleSingleSelection(getCurrentID) {
+    console.log(getCurrentID);
+  }
   return (
     <div className="wrapper">
       <div className="accordion">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div className="item" key={dataItem.id}>
-              <div className="title">
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title"
+              >
                 <h3>{dataItem.question}</h3>
+                <span>+</span>
               </div>
             </div>
           ))
