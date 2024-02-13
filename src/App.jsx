bfa00256a3f9b968a2432e949a944d86a3249bba
Jena <jenagansivakumar@gmail.com>
2024-02-13 16:52:43 +0000

    finished multiselect

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,47 @@ import "./components/accordion/styles.css";
 
 function App() {
   const [selected, setSelected] = useState(null);
+  const [enableMutliSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMutiple] = useState([]);
+
   function handleSingleSelection(getCurrentID) {
     console.log(getCurrentID);
     setSelected(getCurrentID === selected ? null : getCurrentID);
   }
 
-  console.log(selected);
+  function handleMultiSelection(getCurrentID) {
+    let cpyMultiple = [...multiple];
+    const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentID);
+    console.log(findIndexOfCurrentId);
+    if (findIndexOfCurrentId === -1) cpyMultiple.push(getCurrentID);
+    else cpyMultiple.splice(findIndexOfCurrentId, 1);
+    setMutiple(cpyMultiple);
+  }
+
+  console.log(selected, multiple);
   return (
     <div className="wrapper">
-      <button> Enable Multi-Selection</button>
+      <button onClick={() => setEnableMultiSelection(!enableMutliSelection)}>
+        {" "}
+        Enable Multi-Selection
+      </button>
       <div className="accordion">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div className="item" key={dataItem.id}>
               <div
-                onClick={() => handleSingleSelection(dataItem.id)}
+                onClick={
+                  enableMutliSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
                 className="title"
               >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
-              {selected === dataItem.id ? (
+              {selected === dataItem.id ||
+              multiple.indexOf(dataItem.id) !== -1 ? (
                 <div className="content"> {dataItem.answer}</div>
               ) : null}
             </div>
